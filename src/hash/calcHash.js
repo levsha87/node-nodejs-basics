55642bcd9a010d800859843d5f933c070dc663a6
Raman Liausha <Raman_Liausha@epam.com>
2022-12-06 00:03:10 +0300

    fix: correct path hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,8 +1,14 @@
 const { createHmac } = await import('node:crypto');
 import { readFile } from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const calculateHash = async () => {
-    const contents = await readFile("src/hash/files/fileToCalculateHashFor.txt", { encoding: 'utf8' });
+    const contents = await readFile(path.resolve(__dirname, "files", "fileToCalculateHashFor.txt"), { encoding: 'utf8' });
     const hash = createHmac('sha256', contents)
         .digest('hex');
     console.log(hash);
